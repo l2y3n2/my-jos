e07981eb98b4a29d5e89be57096037f0c31998e6
Lin Yuning <l2y3n2@gmail.com>
2011-01-25 14:12:46 +0800

    Lab 3: Exercise 1

--- kern/pmap.c
+++ kern/pmap.c
@@ -184,6 +184,7 @@ i386_vm_init(void)
 	//////////////////////////////////////////////////////////////////////
 	// Make 'envs' point to an array of size 'NENV' of 'struct Env'.
 	// LAB 3: Your code here.
+	envs = boot_alloc(NENV * sizeof(struct Env), PGSIZE);
 
 	//////////////////////////////////////////////////////////////////////
 	// Now that we've allocated the initial kernel data structures, we set
@@ -217,6 +218,9 @@ i386_vm_init(void)
 	//    - the new image at UENVS  -- kernel R, user R
 	//    - envs itself -- kernel RW, user NONE
 	// LAB 3: Your code here.
+	boot_map_segment(pgdir, UENVS,
+			ROUNDUP(NENV * sizeof(struct Env), PGSIZE),
+			PADDR(envs), PTE_U);
 
 	//////////////////////////////////////////////////////////////////////
 	// Use the physical memory that 'bootstack' refers to as the kernel
