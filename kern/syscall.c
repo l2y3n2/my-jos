5e5c87b1949b04c21c59cfe566ec2bec59ebad45
Lin Yuning <l2y3n2@gmail.com>
2011-01-25 16:00:20 +0800

    Lab 4: Exercise 10

--- kern/syscall.c
+++ kern/syscall.c
@@ -334,7 +334,47 @@ static int
 sys_ipc_try_send(envid_t envid, uint32_t value, void *srcva, unsigned perm)
 {
 	// LAB 4: Your code here.
-	panic("sys_ipc_try_send not implemented");
+	struct Env *env;
+	struct Page *pp;
+	pte_t *pte_entry;
+	int r;
+
+	if ((r = envid2env(envid, &env, 0)) < 0)
+		return r;
+
+	if (!(env->env_ipc_recving))
+		return -E_IPC_NOT_RECV;
+
+	if ((uint32_t) srcva < UTOP) {
+		if (PGOFF(srcva))
+			return -E_INVAL;
+		if (!(perm & PTE_U) || !(perm & PTE_P)
+				|| perm != (perm & PTE_USER))
+			return -E_INVAL;
+
+		pte_entry = pgdir_walk(curenv->env_pgdir, srcva, 0);
+		if (pte_entry == NULL)
+			return -E_INVAL;
+		if ((perm & PTE_W) && !(*pte_entry & PTE_W))
+			return -E_INVAL;
+
+		if ((uint32_t) env->env_ipc_dstva < UTOP) {
+			pp = pa2page(PTE_ADDR(*pte_entry));
+			r = page_insert(env->env_pgdir, pp, env->env_ipc_dstva,
+					perm);
+			if (r < 0)
+				return r;
+
+			env->env_ipc_perm = perm;
+		}
+	}
+
+	env->env_ipc_recving = 0;
+	env->env_ipc_from = curenv->env_id;
+	env->env_ipc_value = value;
+	env->env_status = ENV_RUNNABLE;
+
+	return 0;
 }
 
 // Block until a value is ready.  Record that you want to receive
@@ -352,7 +392,19 @@ static int
 sys_ipc_recv(void *dstva)
 {
 	// LAB 4: Your code here.
-	panic("sys_ipc_recv not implemented");
+	struct Env *env;
+	int r;
+
+	if ((uint32_t) dstva < UTOP && PGOFF(dstva))
+		return -E_INVAL;
+
+	curenv->env_ipc_recving = 1;
+	curenv->env_ipc_dstva = dstva;
+	curenv->env_ipc_perm = 0;
+	curenv->env_status = ENV_NOT_RUNNABLE;
+
+	sched_yield();
+
 	return 0;
 }
 
@@ -391,6 +443,10 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
 		case SYS_yield:
 			sys_yield();
 			return 0;
+		case SYS_ipc_try_send:
+			return sys_ipc_try_send(a1, a2, (void *) a3, a4);
+		case SYS_ipc_recv:
+			return sys_ipc_recv((void *) a1);
 		default:
 			return -E_INVAL;
 	}
--- lib/ipc.c
+++ lib/ipc.c
@@ -23,8 +23,26 @@ int32_t
 ipc_recv(envid_t *from_env_store, void *pg, int *perm_store)
 {
 	// LAB 4: Your code here.
-	panic("ipc_recv not implemented");
-	return 0;
+	int r;
+
+	if (pg == NULL)
+		pg = (void *) UTOP;
+
+	r = sys_ipc_recv(pg);
+	if (r < 0) {
+		if (from_env_store)
+			*from_env_store = 0;
+		if (perm_store)
+			*perm_store = 0;
+		return r;
+	}
+
+	if (from_env_store)
+		*from_env_store = env->env_ipc_from;
+	if (perm_store)
+		*perm_store = env->env_ipc_perm;
+
+	return env->env_ipc_value;
 }
 
 // Send 'val' (and 'pg' with 'perm', if 'pg' is nonnull) to 'toenv'.
@@ -39,5 +57,13 @@ void
 ipc_send(envid_t to_env, uint32_t val, void *pg, int perm)
 {
 	// LAB 4: Your code here.
-	panic("ipc_send not implemented");
+	int r;
+
+	if (pg == NULL)
+		pg = (void *) UTOP;
+
+	while ((r = sys_ipc_try_send(to_env, val, pg, perm))) {
+		if (r != -E_IPC_NOT_RECV)
+			panic("sys_ipc_try_send: %e", r);
+	}
 }
