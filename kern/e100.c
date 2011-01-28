8779477bf78457c3f7eeb336e4004d48ed3b8b7c
Lin Yuning <l2y3n2@gmail.com>
2011-01-28 14:59:17 +0800

    Lab 6: Exercise 4

--- kern/e100.c
+++ kern/e100.c
@@ -1,11 +1,21 @@
 // LAB 6: Your driver code here
 
+#include <inc/x86.h>
 #include <kern/pci.h>
 #include <kern/e100.h>
 
 static uint32_t e100_io_addr;
 static uint32_t e100_io_size;
 static uint8_t e100_irq_line;
+
+static void
+delay(void)
+{
+	inb(0x84);
+	inb(0x84);
+	inb(0x84);
+	inb(0x84);
+}
 
 int
 pci_attach_e100(struct pci_func *pcif)
@@ -16,6 +26,10 @@ pci_attach_e100(struct pci_func *pcif)
 	e100_io_addr = pcif->reg_base[1];
 	e100_io_size = pcif->reg_base[1];
 	e100_irq_line = pcif->irq_line;
+
+	outl(e100_io_addr + 8, 0);
+	delay();
+	delay();
 
 	return 1;
 }
